722d89267d00c72b92f6f8f9f49bed4c4183eadf
UrangUrang <ddwax89@gmail.com>
2019-01-05 22:16:12 +0900

    Add post response

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from flask_mock import app
+from flask_stub import app
+from utils import make_views_by_static
 
 if __name__ == '__main__':
+    make_views_by_static()
     app.run(host='0.0.0.0', debug=True, port=5000)
